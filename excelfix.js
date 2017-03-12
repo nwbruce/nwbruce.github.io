d24d71ed47601e8bdc9ccd5cabef1a2f24564d80
Nate Bruce <nwbruce@gmail.com>
2017-03-12 09:00:35 -0400

    Excel fix

--- excelfix.js
+++ excelfix.js
@@ -1,8 +1,22 @@
 
+function fix_it(input) {
+	var output = input.trim();
+	if (output.length >= 2) {
+		if (output[0] == "\"" && output[input.length-1] == "\"") {
+			output = output.substring(1, output.length-1);
+		}
+	}
+
+	output = output.replace(/"\n"/g, "\n\n\n");
+	output = output.replace(/""/g, "\"");
+
+	return output;
+}
+
 var app = angular.module('excelfixApp', []);
 
 app.controller('excelfixControl', function ($scope) {
 	$scope.fix = function() {
-		$scope.output = $scope.input;
+		$scope.output = fix_it($scope.input);
 	}
 });
